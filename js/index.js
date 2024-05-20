1d09c11c68a705390739bdd2cbe4fbfecd723145
Carlos Eugenio <111534932+CarlosEugenio177@users.noreply.github.com>
2024-05-20 11:36:08 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-// Função para verificar se um valor é um número válido
-function isValidNumber(value) {
-    return !isNaN(value) && value !== '';
-}
-
 // Primeiro Formulário: leadCalculatorForm
 document.getElementById("leadCalculatorForm").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -12,7 +7,7 @@ document.getElementById("leadCalculatorForm").addEventListener("submit", functio
     var Leads = parseFloat(document.getElementById("Leads").value);
     var VendasLancamento = parseFloat(document.getElementById("VendasLancamento").value);
 
-    if (!isValidNumber(Ticketproduto) || !isValidNumber(Roas) || !isValidNumber(Leads) || !isValidNumber(VendasLancamento)) {
+    if (isNaN(Ticketproduto) || isNaN(Roas) || isNaN(Leads) || isNaN(VendasLancamento)) {
         alert('Por favor, insira valores válidos para todos os campos.');
         return;
     }
@@ -28,17 +23,18 @@ document.getElementById('QntInvProxLanc_form').addEventListener('submit', functi
     event.preventDefault(); // Impede o envio do formulário
 
     // Obtém os valores de receita e investimento
-    const receita = parseFloat(document.getElementById('Inv_anum').value);
-    const investimento = parseFloat(document.getElementById('Receita_anum').value);
+    const investimento = parseFloat(document.getElementById('Inv_anum').value);
+    const receita = parseFloat(document.getElementById('Receita_anum').value);
 
     // Verifica se os valores são válidos
-    if (!isValidNumber(receita) || !isValidNumber(investimento)) {
+    if (isNaN(receita) || isNaN(investimento)) {
         alert('Por favor, insira valores válidos para receita e investimento.');
         return;
     }
 
     // Calcula o ROAS
-    const roas = parseInt(receita / investimento);
+    const roas = receita / investimento;
+    const lucro = (receita - investimento).toFixed(2);
 
     // Exibe o resultado
     document.getElementById('Roas_id').textContent = roas.toFixed(1);
@@ -48,19 +44,36 @@ document.getElementById('QntInvProxLanc_form').addEventListener('submit', functi
     if (roas < 3) {
         document.getElementById('avisoRoas').style.display = 'block';
         document.getElementById('lucro').style.display = 'none';
+        document.getElementById('reenvestir').style.display = 'none';
+        document.getElementById('total').style.display = 'none';
     } else {
         document.getElementById('avisoRoas').style.display = 'none';
         // Exibe mensagens de lucro conforme o valor do ROAS
         if (roas === 3) {
-            document.getElementById('lucro').textContent = 'Você teve 10% de lucro.';
-        } else if (roas == 5) {
-            document.getElementById('lucro').textContent = 'Você teve 30% de lucro.';
+            const valorReinvestir = (lucro * 0.10).toFixed(2);
+            document.getElementById('lucro').textContent = 'Você obteve 10% de lucro.';
+            document.getElementById('reenvestir').textContent = `Reinvista: R$ ${valorReinvestir}`;
+            document.getElementById('total').textContent = `Total: R$ ${investimento + parseFloat(valorReinvestir)}`; // ParseFloat para converter o valorReinvestir de string para número
+            document.getElementById('lucro').style.display = 'block';
+            document.getElementById('reenvestir').style.display = 'block';
+            document.getElementById('total').style.display = 'block'; // Garante que o elemento 'total' seja exibido
+        } else if (roas === 5) {
+            const valorReinvestir = (lucro * 0.30).toFixed(2);
+            document.getElementById('lucro').textContent = 'Você obteve 30% de lucro.';
+            document.getElementById('reenvestir').textContent = `Reinvista: R$ ${valorReinvestir}`;
+            document.getElementById('total').textContent = `Total: R$ ${investimento + parseFloat(valorReinvestir)}`; // ParseFloat para converter o valorReinvestir de string para número
+            document.getElementById('lucro').style.display = 'block';
+            document.getElementById('reenvestir').style.display = 'block';
+            document.getElementById('total').style.display = 'block'; // Garante que o elemento 'total' seja exibido
         } else {
             document.getElementById('lucro').textContent = '';
+            document.getElementById('reenvestir').style.display = 'none';
+            document.getElementById('total').style.display = 'none';
         }
-        document.getElementById('lucro').style.display = 'block';
     }
+    
 });
+
 document.getElementById("Invlancamento_form").addEventListener("submit", function(event) {
     event.preventDefault(); // Impede o envio do formulário
 
@@ -68,7 +81,7 @@ document.getElementById("Invlancamento_form").addEventListener("submit", functio
     var investimento = parseFloat(document.getElementById("Investimento_T").value);
 
     // Verifica se o valor é válido
-    if (!isValidNumber(investimento)) {
+    if (isNaN(investimento)) {
         alert('Por favor, insira um valor válido para o investimento.');
         return;
     }
@@ -102,3 +115,4 @@ document.getElementById("Invlancamento_form").addEventListener("submit", functio
 
     document.getElementById("resultado_2").style.display = "block";
 });
+
